f3b30c9e681ba02d16ccadb271a7e7c42fdd13f4
Daniel Ordonez <daniel.a.ordonez.r@gmail.com>
2023-12-04 14:11:56 -0600

    +mobile first approach

---
+++ src/lib/Bg.svelte
@@ -0,0 +1,77 @@
+<div id="bg">
+  <div class="shapes"></div>
+</div>
+
+<style>
+  #bg::after,
+  .shapes {
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+  #bg {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+  }
+  #bg::after {
+    z-index: 1;
+    position: absolute;
+    content: "";
+    background-color: rgba(var(--rgb-white), 0.6);
+    backdrop-filter: blur(32px);
+  }
+  .shapes {
+    position: absolute;
+    overflow: hidden;
+    --size: min(1900px, 100vmax);
+  }
+
+  .shapes::before,
+  .shapes:after {
+    position: absolute;
+    content: "";
+  }
+  .shapes::before {
+    width: var(--size);
+    height: var(--size);
+    bottom: 0;
+    left: 0;
+    --bx: -30%;
+    --by: 30%;
+    --ox: -70%;
+    --oy: 60%;
+    transform: translate(-50%, 50%);
+    border-radius: 50%;
+    background-color: rgba(var(--rgb-gray-2), 0.5);
+    animation: offset-xy 10s infinite 5s;
+    animation-fill-mode: both;
+  }
+  .shapes::after {
+    width: var(--size);
+    height: var(--size);
+    top: 0;
+    right: 0;
+    --bx: 30%;
+    --by: -70%;
+    --ox: 70%;
+    --oy: -20%;
+    transform: translate(50%, -50%);
+    border-radius: 50%;
+    background-color: rgba(var(--rgb-white), 0.6);
+    animation: offset-xy 10s infinite;
+    animation-fill-mode: both;
+  }
+  @keyframes offset-xy {
+    from {
+      transform: translate(var(--bx), var(--by)) scale(1);
+    }
+    50% {
+      transform: translate(var(--ox), var(--oy)) scale(1.5);
+    }
+    to {
+      transform: translate(var(--bx), var(--by)) scale(1);
+    }
+  }
+</style>
---
+++ src/lib/ContactLinks.svelte
@@ -0,0 +1,65 @@
+<script>
+  const social = [
+    { icon: "github" },
+    { icon: "linkedin" },
+    { icon: "behance" },
+  ];
+</script>
+
+<div id="contact-links" class="hidden" style="--icons: {social.length}">
+  {#each social as link, i (i)}
+    <a
+      href="#"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="scale-in"
+      style="animation-delay: {1000 + 100 * i}ms;"
+    >
+      <img src="social/{link.icon}.svg" alt="" srcset="" />
+    </a>
+  {/each}
+</div>
+
+<style>
+  #contact-links {
+    --icon-size: 24px;
+    --padding: 4px;
+    --gap: 8px;
+    --border: calc((var(--padding) * 2) + var(--icon-size));
+    --icons-n: 1;
+    --icons: 1;
+    background-color: rgba(var(--rgb-white), 0.8);
+    backdrop-filter: blur(8px);
+    padding: var(--padding) calc(var(--padding) * 2);
+    border-radius: var(--border);
+    display: flex;
+    align-items: center;
+    gap: var(--gap);
+    min-height: var(--border);
+    min-width: var(--border);
+    animation: fade-in--social 400ms ease-in-out 600ms;
+    animation-fill-mode: both;
+    transition: all ease-in-out 400ms;
+    will-change: width, max-width, opacity, transform;
+  }
+  #contact-links a,
+  #contact-links img {
+    height: var(--icon-size);
+    width: var(--icon-size);
+  }
+  @keyframes fade-in--social {
+    0% {
+      opacity: 0;
+      max-width: calc((var(--padding) * 4) + var(--icon-size));
+    }
+    30% {
+      opacity: 1;
+    }
+    100% {
+      max-width: calc(
+        (var(--padding) * 4) + (var(--gap) * (var(--icons) - 1)) +
+          (var(--icon-size) * var(--icons))
+      );
+    }
+  }
+</style>
